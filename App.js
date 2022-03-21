5088e2d000957a04d9ea0e745e3bf2c0906d51d5
adambaj123 <70112272+adambaj123@users.noreply.github.com>
2022-03-21 17:00:21 -0400

    Update App.js

--- App.js
+++ App.js
@@ -12,15 +12,15 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-      screenOptions={{
-        headerShown: false
-      }}>
-        <Stack.Screen name="HomeScreen" component={HomeScreen}/>
-        <Stack.Screen 
-        name="GraphScreen"
-        component={GraphScreen}/>
-        <Stack.Screen name="TransactionScreen" component={TransactionScreen}/>
-        <Stack.Screen name="BudgetScreen" component={BudgetScreen}/>
+        screenOptions={{
+          headerShown: false
+        }}>
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen
+          name="GraphScreen"
+          component={GraphScreen} />
+        <Stack.Screen name="TransactionScreen" component={TransactionScreen} />
+        <Stack.Screen name="BudgetScreen" component={BudgetScreen} />
       </Stack.Navigator>
     </NavigationContainer>
 
@@ -29,207 +29,207 @@ export default function App() {
 const GraphScreen = ({ navigation }) => {
   return (
     <View style={styles.screen}>
-    <View style={styles.topScrn}>
-      <Text style={styles.Text}>Graphs:</Text>
-      <StatusBar style="auto" />
+      <View style={styles.topScrn}>
+        <Text style={styles.Text}>Graphs:</Text>
+        <StatusBar style="auto" />
+      </View>
+      <View style={styles.middleScrn}>
+        <ScrollView style={styles.scrollView}>
+          <PieChart
+            data={PieData}
+            width={screenWidth}
+            height={250}
+            chartConfig={chartConfig}
+            accessor={"population"}
+            backgroundColor={"transparent"}
+            paddingLeft={"15"}
+            absolute
+          />
+          <BarChart
+            data={BarData}
+            width={screenWidth - 20}
+            height={250}
+            yAxisLabel="$"
+            chartConfig={chartConfig}
+            verticalLabelRotation={30}
+            fromZero
+            showValuesOnTopOfBars
+          />
+        </ScrollView>
+      </View>
+      <View style={styles.bottomScrnBoxes}>
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate('HomeScreen')}
+          style={styles.TouchableOpacity}>
+          <Image style={styles.Imagesource} source={require('./logos/4.png')}>
+          </Image>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.bottomScrnBoxes}>
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate('TransactionScreen')}>
+          <Image style={styles.Imagesource} source={require('./logos/1.png')}>
+          </Image>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.bottomScrnBoxes}>
+        <TouchableOpacity>
+          <Image style={styles.Imagesource} source={require('./logos/2.png')}>
+          </Image>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.bottomScrnBoxes}>
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate('BudgetScreen')}>
+          <Image style={styles.Imagesource} source={require('./logos/3.png')}>
+          </Image>
+        </TouchableOpacity>
+      </View>
     </View>
-    <View style={styles.middleScrn}>
-      <ScrollView style={styles.scrollView}>
-      <PieChart
-        data={PieData}
-        width={screenWidth}
-        height={250}
-        chartConfig={chartConfig}
-        accessor={"population"}
-        backgroundColor={"transparent"}
-        paddingLeft={"15"}
-        absolute
-      />
-      <BarChart
-        data={BarData}
-        width={screenWidth -20}
-        height={250}
-        yAxisLabel="$"
-        chartConfig={chartConfig}
-        verticalLabelRotation={30}
-        fromZero
-        showValuesOnTopOfBars
-      />
-    </ScrollView>
-    </View>
-    <View style={styles.bottomScrnBoxes}>
-      <TouchableOpacity 
-      onPress= {() => 
-      navigation.navigate('HomeScreen')}
-      style={styles.TouchableOpacity}>
-      <Image style={styles.Imagesource} source={require('./logos/4.png')}>
-      </Image>
-      </TouchableOpacity>
-    </View>
-    <View style={styles.bottomScrnBoxes}>
-      <TouchableOpacity
-      onPress={() =>
-      navigation.navigate('TransactionScreen')}>
-    <Image style={styles.Imagesource} source={require('./logos/1.png')}>
-      </Image>
-      </TouchableOpacity>
-    </View>
-    <View style={styles.bottomScrnBoxes}>
-      <TouchableOpacity>
-    <Image style={styles.Imagesource} source={require('./logos/2.png')}>
-      </Image>
-      </TouchableOpacity>
-    </View>
-    <View style={styles.bottomScrnBoxes}>
-      <TouchableOpacity
-      onPress={() =>
-      navigation.navigate('BudgetScreen')}>
-    <Image style={styles.Imagesource} source={require('./logos/3.png')}>
-      </Image>
-      </TouchableOpacity>
-    </View>
-  </View>
   )
 }
-const HomeScreen = ({navigation}) => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.screen}>
-    <View style={styles.topLeftScrn}>
-      <Text style={styles.Text}>Welcome, "Name"</Text>
-      <StatusBar style="auto" />
-    </View>
-    <View style={styles.topRightScrn}>
-      <Image style={styles.ProfileImage} source={require('./logos/5.png')}>
+      <View style={styles.topLeftScrn}>
+        <Text style={styles.Text}>Welcome, "Name"</Text>
+        <StatusBar style="auto" />
+      </View>
+      <View style={styles.topRightScrn}>
+        <Image style={styles.ProfileImage} source={require('./logos/5.png')}>
         </Image>
+      </View>
+      <View style={styles.middleScrnHome}>
+        <ScrollView style={styles.scrollView}>
+          <Text>Home Screen Stuff</Text>
+        </ScrollView>
+      </View>
+      <View style={styles.bottomScrnBoxes}>
+        <TouchableOpacity>
+          <Image style={styles.Imagesource} source={require('./logos/4.png')}>
+          </Image>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.bottomScrnBoxes}>
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate('TransactionScreen')}>
+          <Image style={styles.Imagesource} source={require('./logos/1.png')}>
+          </Image>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.bottomScrnBoxes}>
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate('GraphScreen')}>
+          <Image style={styles.Imagesource} source={require('./logos/2.png')}>
+          </Image>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.bottomScrnBoxes}>
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate('BudgetScreen')}>
+          <Image style={styles.Imagesource} source={require('./logos/3.png')}>
+          </Image>
+        </TouchableOpacity>
+      </View>
     </View>
-    <View style={styles.middleScrnHome}>
-      <ScrollView style={styles.scrollView}>
-      <Text>Home Screen Stuff</Text>
-    </ScrollView>
-    </View>
-    <View style={styles.bottomScrnBoxes}>
-      <TouchableOpacity>
-      <Image style={styles.Imagesource} source={require('./logos/4.png')}>
-      </Image>
-      </TouchableOpacity>
-    </View>
-    <View style={styles.bottomScrnBoxes}>
-      <TouchableOpacity
-      onPress={() =>
-      navigation.navigate('TransactionScreen')}>
-    <Image style={styles.Imagesource} source={require('./logos/1.png')}>
-      </Image>
-      </TouchableOpacity>
-    </View>
-    <View style={styles.bottomScrnBoxes}>
-      <TouchableOpacity 
-      onPress= {() => 
-      navigation.navigate('GraphScreen')}>
-    <Image style={styles.Imagesource} source={require('./logos/2.png')}>
-      </Image>
-      </TouchableOpacity>
-    </View>
-    <View style={styles.bottomScrnBoxes}>
-      <TouchableOpacity
-      onPress={() =>
-      navigation.navigate('BudgetScreen')}>
-    <Image style={styles.Imagesource} source={require('./logos/3.png')}>
-      </Image>
-      </TouchableOpacity>
-    </View>
-  </View>
   )
 }
 const TransactionScreen = ({ navigation }) => {
   return (
     <View style={styles.screen}>
-    <View style={styles.topScrn}>
-      <Text style={styles.Text}>Transactions:</Text>
-      <StatusBar style="auto" />
+      <View style={styles.topScrn}>
+        <Text style={styles.Text}>Transactions:</Text>
+        <StatusBar style="auto" />
+      </View>
+      <View style={styles.middleScrn}>
+        <ScrollView style={styles.scrollView}>
+          <Text>Transaction Stuff</Text>
+        </ScrollView>
+      </View>
+      <View style={styles.bottomScrnBoxes}>
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate('HomeScreen')}
+          style={styles.TouchableOpacity}>
+          <Image style={styles.Imagesource} source={require('./logos/4.png')}>
+          </Image>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.bottomScrnBoxes}>
+        <TouchableOpacity>
+          <Image style={styles.Imagesource} source={require('./logos/1.png')}>
+          </Image>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.bottomScrnBoxes}>
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate('GraphScreen')}>
+          <Image style={styles.Imagesource} source={require('./logos/2.png')}>
+          </Image>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.bottomScrnBoxes}>
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate('BudgetScreen')}>
+          <Image style={styles.Imagesource} source={require('./logos/3.png')}>
+          </Image>
+        </TouchableOpacity>
+      </View>
     </View>
-    <View style={styles.middleScrn}>
-      <ScrollView style={styles.scrollView}>
-      <Text>Transaction Stuff</Text>
-    </ScrollView>
-    </View>
-    <View style={styles.bottomScrnBoxes}>
-      <TouchableOpacity 
-      onPress= {() => 
-      navigation.navigate('HomeScreen')}
-      style={styles.TouchableOpacity}>
-      <Image style={styles.Imagesource} source={require('./logos/4.png')}>
-      </Image>
-      </TouchableOpacity>
-    </View>
-    <View style={styles.bottomScrnBoxes}>
-    <TouchableOpacity>
-    <Image style={styles.Imagesource} source={require('./logos/1.png')}>
-      </Image>
-      </TouchableOpacity>
-    </View>
-    <View style={styles.bottomScrnBoxes}>
-    <TouchableOpacity 
-      onPress= {() => 
-      navigation.navigate('GraphScreen')}>
-    <Image style={styles.Imagesource} source={require('./logos/2.png')}>
-      </Image>
-      </TouchableOpacity>
-    </View>
-    <View style={styles.bottomScrnBoxes}>
-    <TouchableOpacity
-      onPress={() =>
-      navigation.navigate('BudgetScreen')}>
-    <Image style={styles.Imagesource} source={require('./logos/3.png')}>
-      </Image>
-      </TouchableOpacity>
-    </View>
-  </View>
   )
 }
 const BudgetScreen = ({ navigation }) => {
   return (
     <View style={styles.screen}>
-    <View style={styles.topScrn}>
-      <Text style={styles.Text}>Budget "What if":</Text>
-      <StatusBar style="auto" />
+      <View style={styles.topScrn}>
+        <Text style={styles.Text}>Budget "What if":</Text>
+        <StatusBar style="auto" />
+      </View>
+      <View style={styles.middleScrn}>
+        <ScrollView style={styles.scrollView}>
+          <Text>Budget Stuff</Text>
+        </ScrollView>
+      </View>
+      <View style={styles.bottomScrnBoxes}>
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate('HomeScreen')}
+          style={styles.TouchableOpacity}>
+          <Image style={styles.Imagesource} source={require('./logos/4.png')}>
+          </Image>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.bottomScrnBoxes}>
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate('TransactionScreen')}>
+          <Image style={styles.Imagesource} source={require('./logos/1.png')}>
+          </Image>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.bottomScrnBoxes}>
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate('GraphScreen')}>
+          <Image style={styles.Imagesource} source={require('./logos/2.png')}>
+          </Image>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.bottomScrnBoxes}>
+        <TouchableOpacity>
+          <Image style={styles.Imagesource} source={require('./logos/3.png')}>
+          </Image>
+        </TouchableOpacity>
+      </View>
     </View>
-    <View style={styles.middleScrn}>
-      <ScrollView style={styles.scrollView}>
-      <Text>Budget Stuff</Text>
-    </ScrollView>
-    </View>
-    <View style={styles.bottomScrnBoxes}>
-      <TouchableOpacity 
-      onPress= {() => 
-      navigation.navigate('HomeScreen')}
-      style={styles.TouchableOpacity}>
-      <Image style={styles.Imagesource} source={require('./logos/4.png')}>
-      </Image>
-      </TouchableOpacity>
-    </View>
-    <View style={styles.bottomScrnBoxes}>
-      <TouchableOpacity
-      onPress={() =>
-      navigation.navigate('TransactionScreen')}>
-    <Image style={styles.Imagesource} source={require('./logos/1.png')}>
-      </Image>
-      </TouchableOpacity>
-    </View>
-    <View style={styles.bottomScrnBoxes}>
-      <TouchableOpacity 
-      onPress= {() => 
-      navigation.navigate('GraphScreen')}>
-    <Image style={styles.Imagesource} source={require('./logos/2.png')}>
-      </Image>
-      </TouchableOpacity>
-    </View>
-    <View style={styles.bottomScrnBoxes}>
-      <TouchableOpacity>
-    <Image style={styles.Imagesource} source={require('./logos/3.png')}>
-      </Image>
-      </TouchableOpacity>
-    </View>
-  </View>
   )
 }
 const styles = StyleSheet.create({
@@ -238,8 +238,8 @@ const styles = StyleSheet.create({
     flexWrap: "wrap",
     height: '100%',
   },
-  topScrn:{
-    paddingTop:50,
+  topScrn: {
+    paddingTop: 50,
     width: '100%',
     height: '10%',
     backgroundColor: '#33d651',
@@ -286,7 +286,7 @@ const styles = StyleSheet.create({
   Imagesource: {
     height: 100,
     width: 102,
-    resizeMode:'stretch',
+    resizeMode: 'stretch',
   },
   ProfileImage: {
     height: 90,

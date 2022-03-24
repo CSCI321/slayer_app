29f2681cec7e6a481bc2368d4b1c7eff288cf99c
Matthew Stefanovic <mstefanovic@stetson.edu>
2022-03-23 22:44:30 -0400

    Added plaid enviroment

--- App.js
+++ App.js
@@ -6,9 +6,41 @@ import {
 } from "react-native-chart-kit";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useState, useEffect } from 'react';
+import { usePlaidLink } from 'react-plaid-link';
+import { saveBalances, getBalances, getLinkToken } from "./Data";
+const axios = require('axios');
 
 const Stack = createNativeStackNavigator();
 export default function App() {
+  /*
+    ------------------------------- Start Plaid --------------------------------
+           To call the Plaid API popup box make sure to use "open()" hook
+                            in the return statement.
+  */
+
+  const [linkToken, setLinkToken] = useState(null);
+  useEffect(() => {
+    getLinkToken().then(lt => setLinkToken(lt));
+  }, [])
+  const [accessToken, setAccessToken] = useState(null);
+  const [balance, setBalance] = useState(null);
+
+  const { open, ready } = usePlaidLink({
+    token: linkToken,
+    onSuccess: (public_token, metadata) => {
+      axios.post("https://birdboombox.com/api/exchange_public_token",
+          {"public_token": public_token})
+          .then(response => setAccessToken(response.data.access_token));
+    }
+  });
+
+  useEffect(() => {
+    if(accessToken) {
+      getBalances(accessToken).then(b => setBalance(b));
+    }
+  }, [accessToken]);
+
   return (
     <NavigationContainer>
       <Stack.Navigator
---
+++ Data.js
@@ -0,0 +1,37 @@
+const BALANCE_NAME = 'Balances';
+const ACCESS_TOKEN_NAME = 'Access Token';
+const API_URL = "https://birdboombox.com/api/";
+const axios = require('axios');
+
+// Saves the balances to localStorage
+export function saveBalances(balance) {
+    localStorage.setItem(BALANCE_NAME, JSON.stringify(balance.Balance.accounts));
+}
+
+// Gets the balances from Plaid Servers
+export function getBalances(accessToken) {
+    if(accessToken) {
+        return axios.post(API_URL + "getBalance",
+            {"access_token": accessToken})
+            .then(response => response.data.Balance.accounts);
+    } else {
+        return new Promise(() => null);
+    }
+}
+
+// Gets the balances saved to localStorage
+export function getSavedBalances() {
+    let balance = JSON.stringify(localStorage.getItem(BALANCE_NAME));
+    return { balance }
+}
+
+// Gets the Link_Token from Plaid Servers
+export function getLinkToken() {
+    return axios.get(API_URL + "create_link_token")
+        .then(response => response.data.link_token);
+}
+
+//Saves the Access_Token to localStorage
+function saveAccessToken(accessToken) {
+    localStorage.setItem(ACCESS_TOKEN_NAME, JSON.stringify(accessToken))
+}

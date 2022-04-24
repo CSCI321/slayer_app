ea8044b55ef30ea54af9757c4d43ab66849e1a51
snxhz01 <77512847+snxhz01@users.noreply.github.com>
2022-04-24 13:55:33 -0400

    budget screen

--- App.js
+++ App.js
@@ -203,7 +203,44 @@ const TransactionScreen = ({ navigation }) => {
 }
 const BudgetScreen = ({ navigation }) => {
   return (
-  <></>
+    <View style={styles.whiteBackground}>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Refinance</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarColor01">
+            <ul class="navbar-nav me-auto">
+              <li class="nav-item">
+                <a class="nav-link active" href="#">Home
+                  <span class="visually-hidden">(current)</span>
+                </a>
+              </li>
+              <li class="nav-item">
+              <TouchableOpacity 
+              onPress={() =>
+            navigation.navigate('TransactionScreen')}
+          style={styles.TouchableOpacity}>
+                <a class="nav-link" href="#">Transaction</a>
+                </TouchableOpacity>
+              </li>
+              
+              <li class="nav-item">
+                <a class="nav-link" href="#">Graphs</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Budget: What if</a>
+              </li>
+            </ul>
+            <form class="d-flex">
+              <button type="button" class="btn btn-primary">Profile</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </View> 
   )
 }
 const styles = StyleSheet.create({

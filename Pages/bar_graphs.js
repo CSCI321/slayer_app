653d1b3e31211a94489a707503de13363656ade3
adambaj123 <70112272+adambaj123@users.noreply.github.com>
2022-05-02 14:03:17 -0400

    Bar Graph Complete

--- Pages/bar_graphs.js
+++ Pages/bar_graphs.js
@@ -83,6 +83,7 @@ function Bar_graphs() {
             }
             console.log('Date:', date);
         }
+        
     );
     console.log('January', janAmount);
     console.log('February', febAmount);
@@ -104,33 +105,47 @@ function Bar_graphs() {
             }}>
                 Refresh
             </Button>{' '}
+            <View>
             <VictoryChart
-                // domainPadding will add space to each side of VictoryBar to
-                // prevent it from overlapping the axis
-                theme={VictoryTheme.material}
-                domainPadding={20}
-                height={300}
-                width={800}
-            >
-                <VictoryAxis
-                    // tickValues specifies both the number of ticks and where
-                    // they are placed on the axis
-                    tickValues={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]}
-                    tickFormat={["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]}
-                />
-                <VictoryAxis
-                    dependentAxis
-                    // tickFormat specifies how ticks should be displayed
-                    tickFormat={(x) => (`$${x / 10}`)}
-                />
-                <VictoryBar
-
-                    barWidth={({ index }) => index * 2 + 30}
-                    data={data}
-                    x="quarter"
-                    y="earnings"
-                />
-            </VictoryChart>
+                    // domainPadding will add space to each side of VictoryBar to
+                    // prevent it from overlapping the axis
+                    theme={VictoryTheme.material}
+                    domainPadding={20}
+                    height={300}
+                    width={800}
+                >
+                    <VictoryAxis
+                        // tickValues specifies both the number of ticks and where
+                        // they are placed on the axis
+                        tickValues={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]}
+                        tickFormat={["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]}
+                    />
+                    <VictoryAxis
+                        dependentAxis
+                        // tickFormat specifies how ticks should be displayed
+                        tickFormat={1}
+                    />
+                    <VictoryBar
+    
+                        barWidth={({ index }) => index * 2 + 30}
+                        data={
+                        [{ quarter: 1, earnings: Math.abs(janAmount) },
+                        { quarter: 2, earnings: Math.abs(febAmount) },
+                        { quarter: 3, earnings: Math.abs(marAmount) },
+                        { quarter: 4, earnings: Math.abs(aprAmount) },
+                        { quarter: 5, earnings: Math.abs(mayAmount) },
+                        { quarter: 6, earnings: Math.abs(junAmount) },
+                        { quarter: 7, earnings: Math.abs(julAmount) },
+                        { quarter: 8, earnings: Math.abs(augAmount) },
+                        { quarter: 9, earnings: Math.abs(sepAmount) },
+                        { quarter: 10, earnings: Math.abs(octAmount) },
+                        { quarter: 11, earnings: Math.abs(novAmount) },
+                        { quarter: 12, earnings: Math.abs(decAmount) }]}
+                        x="quarter"
+                        y="earnings"
+                    />
+                </VictoryChart>
+            </View>
         </View>
     );
 }
@@ -146,18 +161,18 @@ const chartConfig = {
     useShadowColorFromDataset: false, // optional
 };
 const data = [
-    { quarter: 1, earnings: 500 },
-    { quarter: 2, earnings: 600 },
-    { quarter: 3, earnings: 250 },
-    { quarter: 4, earnings: 130 },
-    { quarter: 5, earnings: 100 },
-    { quarter: 6, earnings: 900 },
-    { quarter: 7, earnings: 120 },
-    { quarter: 8, earnings: 800 },
-    { quarter: 9, earnings: 500 },
-    { quarter: 10, earnings: 300 },
-    { quarter: 11, earnings: 200 },
-    { quarter: 12, earnings: 1000 },
+    { quarter: 1, earnings:  0},
+    { quarter: 2, earnings:  0},
+    { quarter: 3, earnings:  0},
+    { quarter: 4, earnings:  0},
+    { quarter: 5, earnings:  0},
+    { quarter: 6, earnings:  0},
+    { quarter: 7, earnings:  0},
+    { quarter: 8, earnings:  0},
+    { quarter: 9, earnings:  0},
+    { quarter: 10, earnings: 0},
+    { quarter: 11, earnings: 0},
+    { quarter: 12, earnings: 0},
 ];
 const styles = StyleSheet.create({
     homeBudget: {
@@ -169,4 +184,36 @@ const styles = StyleSheet.create({
         backgroundColor: "white"
     }
 });
+function updateChart(){
+    return(
+    <VictoryChart
+    // domainPadding will add space to each side of VictoryBar to
+    // prevent it from overlapping the axis
+    theme={VictoryTheme.material}
+    domainPadding={20}
+    height={300}
+    width={800}
+>
+    <VictoryAxis
+        // tickValues specifies both the number of ticks and where
+        // they are placed on the axis
+        tickValues={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]}
+        tickFormat={["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]}
+    />
+    <VictoryAxis
+        dependentAxis
+        // tickFormat specifies how ticks should be displayed
+        tickFormat={(x) => (`$${x / 10}`)}
+    />
+    <VictoryBar
+
+        barWidth={({ index }) => index * 2 + 30}
+        data={updatedata}
+        x="quarter"
+        y="earnings"
+    />
+</VictoryChart>
+    )
+}
+
 export default Bar_graphs;

a5ce16d67be59dce7750fbe619f13d1e992ca36c
snxhz01 <77512847+snxhz01@users.noreply.github.com>
2022-05-02 02:56:07 -0400

    Update Budget.js

--- Pages/Budget.js
+++ Pages/Budget.js
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View, Dimensions, ScrollView, Image, TouchableOpacity, Button } from 'react-native';
 import "bootswatch/dist/yeti/bootstrap.min.css";
 import React from 'react';
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme} from 'victory';
 
 export default class Budget extends React.Component {
     render(){
@@ -12,7 +13,6 @@ export default class Budget extends React.Component {
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
-  
             <div class="collapse navbar-collapse" id="navbarColor01">
               <ul class="navbar-nav me-auto">
               <li class="nav-item">
@@ -36,7 +36,7 @@ export default class Budget extends React.Component {
                 <TouchableOpacity 
                 onPress={() =>
               this.props.navigation.navigate({ name: 'GraphScreen'})}
-            style={styles.TouchableOpacity}>
+              style={styles.TouchableOpacity}>
                   <a class="nav-link" href="#">Graphs</a>
                   </TouchableOpacity>
                   
@@ -53,6 +53,57 @@ export default class Budget extends React.Component {
             </div>
           </div>
         </nav>
+
+        <div>
+
+          
+         <h1>Budget</h1> 
+          <View style={{
+            height: 50, width: 100, marginTop: 15,
+            gap:4, flexDirection: "row", alignContent: 'center'
+          }}>
+
+            <TouchableOpacity>
+            <Button title="Edit Budget" />
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+            <Button title="What-if"/>  
+            </TouchableOpacity>
+          </View>
+          
+          <View>
+          <VictoryChart
+                // domainPadding will add space to each side of VictoryBar to
+                // prevent it from overlapping the axis
+                theme={VictoryTheme.material}
+                domainPadding={20}
+                height ={300}
+                width ={800}
+              >
+                <VictoryAxis
+                  // tickValues specifies both the number of ticks and where
+                  // they are placed on the axis
+                  tickValues={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]}
+                  tickFormat={["Jan", "Feb", "Mar", "Apr","May", "Jun", "Jul","Aug","Sep","Oct","Nov","Dec"]}
+                />
+                <VictoryAxis
+                  dependentAxis
+                  // tickFormat specifies how ticks should be displayed
+                  tickFormat={(x) => (`$${x / 10}`)}
+                />
+                <VictoryBar
+                
+                barWidth={({ index }) => index * 2 + 30}
+                  data={data}
+                  x="quarter"
+                  y="earnings"
+                />
+              </VictoryChart>
+          </View>  
+
+        </div>
+      
       </View>
     )
     }
@@ -65,6 +116,14 @@ export default class Budget extends React.Component {
     },
     whiteBackground: {
       backgroundColor: "white"
-    }
+    },
+    button: {
+     height: 50, 
+     width: 50, 
+     alignItems: 'center',
+     justifyContent: 'center',
+     marginTop: 50,
+     elevation:3,
+    },
   });
-  const screenWidth = Dimensions.get("window").width;
+const screenWidth = Dimensions.get("window").width;
